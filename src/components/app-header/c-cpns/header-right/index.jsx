5a6446dd0d60989dd1857f96af7d029cea8dc230
callie <chzx_dqy@outlook.com>
2023-09-19 09:11:01 +0800

    feat: header区域Profile点击面板的切换效果

--- src/components/app-header/c-cpns/header-right/index.jsx
+++ src/components/app-header/c-cpns/header-right/index.jsx
@@ -1,21 +1,59 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { RightWrapper } from "./style";
 import IconGlobal from "@/assets/svg/icon_global";
 import IconMenu from "@/assets/svg/icon_menu";
 import IconAvatar from "@/assets/svg/icon_avatar";
 
 const HeaderRight = memo(() => {
+    /** 定义组件内部状态 */
+  const [showPanel, setShowPanel] = useState(false);
+
+  /** 事件处理函数 */
+  function profileClickHandle() {
+    setShowPanel(true)
+  }
+
+  /** 副作用代码 */
+  useEffect(()=>{
+    function windowClickHandle() {
+        console.log(1);
+        setShowPanel(false)
+    }
+
+    window.addEventListener('click',windowClickHandle,true)
+
+    function removeClick() {
+        window.removeEventListener('click',windowClickHandle,true)
+    }
+    return removeClick
+  },[])
+
   return (
     <RightWrapper className="right">
       <div className="btns">
         <span className="btn">登录</span>
         <span className="btn">注册</span>
-        <span className="btn"><IconGlobal/></span>
+        <span className="btn">
+          <IconGlobal />
+        </span>
       </div>
 
-      <div className="profile">
-        <IconMenu/>
-        <IconAvatar/>
+      <div className="profile" onClick={profileClickHandle}>
+        <IconMenu />
+        <IconAvatar />
+        {showPanel && (
+          <div className="panel">
+            <div className="top">
+              <div className="item register">注册</div>
+              <div className="item login">登录</div>
+            </div>
+            <div className="bottom">
+              <div className="item">出租房源</div>
+              <div className="item">开展体验</div>
+              <div className="item">帮助</div>
+            </div>
+          </div>
+        )}
       </div>
     </RightWrapper>
   );
--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const RightWrapper = styled.div`
   flex: 1;
   display: flex;
@@ -34,6 +33,35 @@ export const RightWrapper = styled.div`
     background-color: #fff;
     color: ${(props) => props.theme.text.primaryColor};
     cursor: pointer;
-    ${props => props.theme.mixin.boxShadow};
+    ${(props) => props.theme.mixin.boxShadow};
+    .panel {
+      position: absolute;
+      top: 54px;
+      right: 0;
+      width: 240px;
+      background-color: #fff;
+      border-radius: 10px;
+      box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
+      color: #666;
+
+      .top,
+      .bottom {
+        padding: 10px 0;
+
+        .item {
+          height: 40px;
+          line-height: 40px;
+          padding: 0 16px;
+
+          &:hover {
+            background-color: #f5f5f5;
+          }
+        }
+      }
+
+      .top {
+        border-bottom: 1px solid #ddd;
+      }
+    }
   }
 `;
