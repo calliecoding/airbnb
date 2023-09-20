7fdf99f2ca8791f7d3f86c83871cffc079737e6c
callie <chzx_dqy@outlook.com>
2023-09-21 02:37:09 +0800

    feat: 全部-全部页面的跳转和基础布局搭建

--- src/components/section-footer/index.jsx
+++ src/components/section-footer/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React, { memo } from "react";
 import { FooterWrapper } from "./style";
 import IconMoreArrow from "@/assets/svg/icon-more-arrow";
+import { useNavigate } from "react-router-dom";
 
 const SectionFooter = memo((props) => {
   const { name } = props;
@@ -15,9 +16,14 @@ const SectionFooter = memo((props) => {
   const customStyle = {
     color: name ? "#00848A" : "#000",
   };
+    /** 事件处理 */
+    const navigate = useNavigate()
+  function moreHandle(params) {
+    navigate("/entire")
+  }
   return (
     <FooterWrapper {...customStyle}>
-      <div className="info">
+      <div className="info" onClick={moreHandle}>
         <div className="text">{message}</div>
         <div className="icon">
           <IconMoreArrow />
