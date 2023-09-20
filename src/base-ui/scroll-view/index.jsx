0c777ac88bfec87abd65df8a5b3c4d34dcd2f684
callie <chzx_dqy@outlook.com>
2023-09-20 22:51:59 +0800

    feat: 首页-ScrollView右边按钮点击向左滚动

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -5,21 +5,38 @@ import { ViewWrapper } from "./style";
 const ScrollView = memo((props) => {
   /** 定义内部状态 */
   const [showRight, setShowRight] = useState(false);
-
+  const [posIndex, setPosIndex] = useState(0); // 距离定位父级的偏移
+  const totalDistanceRef = useRef()
+    
   /** 组件渲染完毕后，判断是否显示右侧按钮*/
   const srcollContentRef = useRef(null);
   useEffect(() => {
     const scrollWidth = srcollContentRef.current.scrollWidth; //元素的整体宽度
     const clientWidth = srcollContentRef.current.clientWidth;
+    totalDistanceRef.current = scrollWidth - clientWidth;
 
-    const totalDiatance = scrollWidth - clientWidth;
-
-    setShowRight(totalDiatance>0)
+    setShowRight(totalDistanceRef.current > 0);
   }, [props.children]);
+
+  function rightClickHandle(params) {
+    const newIndex = posIndex + 1;
+    setPosIndex(newIndex)
+    const newEl =  srcollContentRef.current.children[newIndex]
+    const newEloffSetLeft = newEl.offsetLeft
+    console.log(newEl);
+    srcollContentRef.current.style.transform = `translate(-${newEloffSetLeft}px)`
+   
+    // 判断是否继续显示右边按钮
+    setShowRight(totalDistanceRef.current > newEloffSetLeft);
+  }
   return (
     <ViewWrapper>
       <div className="left">左-----</div>
-      {showRight && <div className="right">右======</div>}
+      {showRight && (
+        <div className="right" onClick={rightClickHandle}>
+          右======
+        </div>
+      )}
       <div className="scroll-content" ref={srcollContentRef}>
         {props.children}
       </div>
--- src/base-ui/scroll-view/style.js
+++ src/base-ui/scroll-view/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const ViewWrapper = styled.div`
+    position: relative;
     /* display: flex; */
     overflow: hidden;
     > div{
@@ -8,5 +9,6 @@ export const ViewWrapper = styled.div`
     }
   .scroll-content {
     display: flex;
+    transition: transform 200ms ease;
   }
 `;
