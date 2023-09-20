6d5c035c49da7cb668743ae4ca4cd18f9d17a107
callie <chzx_dqy@outlook.com>
2023-09-20 11:58:41 +0800

    feat: 首页-房间item的布局

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -5,8 +5,21 @@ import { ItemWrapper } from "./style";
 const RoomItem = memo((props) => {
   const { itemData } = props;
   return (
-    <ItemWrapper >
-      <li> {itemData.name}</li>;
+    <ItemWrapper verifycolor={itemData?.verify_info?.text_color || "#39576a"}>
+        <div className="inner">
+            <div className="cover">
+                <img src={itemData.picture_url} alt="" />
+            </div>
+            <div className="desc">
+                {itemData.verify_info.messages.join('·')}
+            </div>
+            <div className="name">
+                {itemData.name}
+            </div>
+            <div className="price">
+                {itemData.price_format}/晚
+            </div>
+        </div>
     </ItemWrapper>
   )
 });
