dda8edf1b11f49f7208a858da3991d745f17d5d9
callie <chzx_dqy@outlook.com>
2023-09-21 00:03:33 +0800

    feat: 首页-ScrollView左侧按钮及样式调整

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import React, { memo, useEffect, useRef, useState } from "react";
 import { ViewWrapper } from "./style";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
 
 const ScrollView = memo((props) => {
   /** 定义内部状态 */
-  const [showRight, setShowRight] = useState(false);
+  const [showRight, setShowRight] = useState(false); // 右边按钮
+  const [showLeft, setShowLeft] = useState(false); // 左边按钮
   const [posIndex, setPosIndex] = useState(0); // 距离定位父级的偏移
-  const totalDistanceRef = useRef()
-    
+  const totalDistanceRef = useRef();
+
   /** 组件渲染完毕后，判断是否显示右侧按钮*/
   const srcollContentRef = useRef(null);
   useEffect(() => {
@@ -19,27 +22,59 @@ const ScrollView = memo((props) => {
   }, [props.children]);
 
   function rightClickHandle(params) {
+    console.log(11);
     const newIndex = posIndex + 1;
-    setPosIndex(newIndex)
-    const newEl =  srcollContentRef.current.children[newIndex]
-    const newEloffSetLeft = newEl.offsetLeft
-    console.log(newEl);
-    srcollContentRef.current.style.transform = `translate(-${newEloffSetLeft}px)`
-   
+    setPosIndex(newIndex);
+    const newEl = srcollContentRef.current.children[newIndex];
+    const newElOffSetLeft = newEl.offsetLeft;
+    srcollContentRef.current.style.transform = `translate(-${newElOffSetLeft}px)`;
+
     // 判断是否继续显示右边按钮
-    setShowRight(totalDistanceRef.current > newEloffSetLeft);
+    setShowRight(totalDistanceRef.current > newElOffSetLeft);
+    setShowLeft(newElOffSetLeft > 0);
   }
+
+  function controlClickHandle(isRight) {
+    const newIndex = isRight ? posIndex + 1 : posIndex - 1;
+    setPosIndex(newIndex);
+    const newEl = srcollContentRef.current.children[newIndex];
+    const newElOffSetLeft = newEl.offsetLeft;
+
+    srcollContentRef.current.style.transform = `translate(-${newElOffSetLeft}px)`;
+    // 判断是否继续显示右边按钮
+    setShowRight(totalDistanceRef.current > newElOffSetLeft);
+    setShowLeft(newElOffSetLeft > 0);
+  }
+
   return (
     <ViewWrapper>
-      <div className="left">左-----</div>
-      {showRight && (
-        <div className="right" onClick={rightClickHandle}>
-          右======
+      {showLeft && (
+        <div
+          className="control left"
+          onClick={() => {
+            controlClickHandle(false);
+          }}
+        >
+          <IconArrowLeft />
         </div>
       )}
+      {showRight && (
+        <div
+          className="control right"
+          onClick={() => {
+            controlClickHandle(true);
+          }}
+        >
+          <IconArrowRight />
+        </div>
+      )}
+
+      <div className="srcoll">
       <div className="scroll-content" ref={srcollContentRef}>
         {props.children}
       </div>
+      </div>
+    
     </ViewWrapper>
   );
 });
