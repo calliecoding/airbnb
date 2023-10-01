eb23cb69f2bc7a7a196dc1ed42ac97ab5086ef53
callie <chzx_dqy@outlook.com>
2023-10-01 23:51:34 +0800

    feat: 全部页面-请求数据的蒙版展示

--- src/store/modeules/entire/createActions.js
+++ src/store/modeules/entire/createActions.js
@@ -16,21 +16,29 @@ export const changeTotalCountAction = (totalCount) => ({
   totalCount,
 });
 
+export const changeIsLoadingAction = (isLoading) => ({
+  type: actionTypes.CHANGE_IS_LOADING,
+  isLoading,
+});
+
 export const fetchRoomListAction = (page = 0) => {
   // 新的函数
   return async (dispatch, getState) => {
     // 0.修改currentPage
-    dispatch(changeCurrentPageAction(page))
+    dispatch(changeCurrentPageAction(page));
 
     // 根据页码获取最新的数据
-    // const currentPage = getState().entire.currentPage    
+    // const currentPage = getState().entire.currentPage
 
-   const res = await getEntireRoomList(page * 20)
+    dispatch(changeIsLoadingAction(true));
+    const res = await getEntireRoomList(page * 20);
+
+    dispatch(changeIsLoadingAction(false));
     // 获取到最新的数据, 保存redux的store中
-   const roomList = res.list
-   const totalCount = res.totalCount
+    const roomList = res.list;
+    const totalCount = res.totalCount;
 
-   dispatch(changeRoomListAction(roomList))
-   dispatch(changeTotalCountAction(totalCount))
+    dispatch(changeRoomListAction(roomList));
+    dispatch(changeTotalCountAction(totalCount));
   };
 };
--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,23 +1,31 @@
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import React, { memo } from "react";
 import { RoomsWrapper } from "./style";
 import { shallowEqual, useSelector } from "react-redux";
 import RoomItem from "@/components/room-item";
 
 const EntireRooms = memo((props) => {
-  const { roomList, totalCount } = useSelector((state) => ({
-    roomList: state.entire.roomList,
-    totalCount: state.entire.totalCount,
-  }),shallowEqual);
+
+  const { roomList, totalCount,isLoading } = useSelector(
+    (state) => ({
+      roomList: state.entire.roomList,
+      totalCount: state.entire.totalCount,
+      isLoading: state.entire.isLoading,
+      
+    }),
+    shallowEqual
+  );
   return (
     <RoomsWrapper>
       <h2 className="title">{totalCount}多处住所</h2>
 
       <div className="list">
-        {!!roomList.length&&roomList.map((item) => {
-          return <RoomItem key={item.id} itemData={item} itemWidth="20%" />;
-        })}
+        {!!roomList.length &&
+          roomList.map((item) => {
+            return <RoomItem key={item._id} itemData={item} itemWidth="20%" />;
+          })}
       </div>
+      {isLoading && <div className="cover"></div>}
     </RoomsWrapper>
   );
 });
