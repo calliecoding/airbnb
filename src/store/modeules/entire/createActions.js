af2d73c2cd0280a47538610313d20a223a4e0cd7
callie <chzx_dqy@outlook.com>
2023-10-01 16:46:12 +0800

    feat: 全部页面-根据页码获取最新的数据

--- src/store/modeules/entire/createActions.js
+++ src/store/modeules/entire/createActions.js
@@ -19,7 +19,12 @@ export const changeTotalCountAction = (totalCount) => ({
 export const fetchRoomListAction = (page = 0) => {
   // 新的函数
   return async (dispatch, getState) => {
-   const res = await getEntireRoomList(0)
+
+    // 根据页码获取最新的数据
+    const currentPage = getState().entire.currentPage
+    console.log(currentPage);
+
+   const res = await getEntireRoomList(currentPage * 20)
     // 获取到最新的数据, 保存redux的store中
    const roomList = res.list
    const totalCount = res.totalCount
