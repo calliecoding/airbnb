edca825849816e89a678740d43f5ecd3e5ac5dc1
callie <chzx_dqy@outlook.com>
2023-10-09 07:35:46 +0800

    feat: 全部页面-RoomItem的轮播图实现效果

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,24 +1,62 @@
 import PropTypes from "prop-types";
-import React, { memo } from "react";
+import React, { memo, useRef, useState } from "react";
 import Rating from "@mui/material/Rating";
+import { Carousel } from "antd";
 
 import { ItemWrapper } from "./style";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
 
 const RoomItem = memo((props) => {
-  const { itemData, itemWidth} = props;
+  const { itemData, itemWidth } = props;
 
   // 动态样式
   const customStyle = {
     verifycolor: itemData?.verify_info?.text_color || "#39576a",
     contentcolor: itemData?.bottom_info?.content_color || "green",
-    itemwidth:itemWidth,
+    itemwidth: itemWidth,
   };
+
+  const sliderRef = useRef();
+
+  /** 事件处理的逻辑 */
+  function controlClickHandle(isRight = true) {
+    // 上一个面板/下一个面板
+    isRight ? sliderRef.current.next() : sliderRef.current.prev();
+  }
   return (
     <ItemWrapper {...customStyle}>
       <div className="inner">
-        <div className="cover">
+        {/* <div className="cover">
           <img src={itemData.picture_url} alt="" />
+        </div> */}
+
+        <div className="slider">
+          <div className="controls">
+            <div
+              className="btn left"
+              onClick={(e) => controlClickHandle(false)}
+            >
+              <IconArrowLeft height="30" width="30" />
+            </div>
+            <div
+              className="btn right"
+              onClick={(e) => controlClickHandle(true)}
+            >
+              <IconArrowRight height="30" width="30" />
+            </div>
+          </div>
+          <Carousel dots={false} ref={sliderRef}>
+            {itemData?.picture_urls?.map((item) => {
+              return (
+                <div className="cover" key={item}>
+                  <img src={item} alt="" />
+                </div>
+              );
+            })}
+          </Carousel>
         </div>
+
         <div className="desc">{itemData.verify_info.messages.join("·")}</div>
         <div className="name">{itemData.name}</div>
         <div className="price">{itemData.price_format}/晚</div>
@@ -42,7 +80,7 @@ const RoomItem = memo((props) => {
 
 RoomItem.propTypes = {
   itemData: PropTypes.object,
-  itemWidth:PropTypes.string
+  itemWidth: PropTypes.string,
 };
 
 export default RoomItem;
