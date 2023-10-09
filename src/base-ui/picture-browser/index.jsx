8988779bc304dd3fa7b5af3c0567855655cc2c5d
callie <chzx_dqy@outlook.com>
2023-10-09 13:06:58 +0800

    wip: 详情页面-图片浏览器的中间图片显示

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -2,9 +2,13 @@ import PropTypes from "prop-types";
 import React, { memo, useEffect } from "react";
 import { BrowserWrapper } from "./style";
 import IconClose from "@/assets/svg/icon-close";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
+import { useState } from "react";
 
 const PictureBrowser = memo((props) => {
   const { pictureUrls, closeClick } = props;
+  const [currentIndex, setCurrentIndex] = useState(0)
   // 当图片浏览器展示出来时, 滚动的功能消失
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -17,6 +21,13 @@ const PictureBrowser = memo((props) => {
    function closeBtnClickHandle() {
     if (closeClick) closeClick()
   }
+  function controlClickHandle(isNext = true) {
+    let newIndex = isNext ? currentIndex + 1: currentIndex - 1
+    if (newIndex < 0) newIndex = pictureUrls.length - 1
+    if (newIndex > pictureUrls.length - 1) newIndex = 0
+
+    setCurrentIndex(newIndex)
+  }
   return (
     <BrowserWrapper>
       <div className="top">
@@ -24,6 +35,17 @@ const PictureBrowser = memo((props) => {
          <IconClose/>
         </div>
       </div>
+      <div className="slider">
+      <div className='controls'>
+          <div className='btn left' onClick={e => controlClickHandle(false)}>
+            <IconArrowLeft width="77" height="77"/>
+          </div>
+          <div className='btn right' onClick={e => controlClickHandle(true)}>
+            <IconArrowRight width="77" height="77"/>
+          </div>
+        </div>
+      </div>
+      <div className="preview"></div>
     </BrowserWrapper>
   );
 });
--- src/base-ui/picture-browser/style.js
+++ src/base-ui/picture-browser/style.js
@@ -24,4 +24,57 @@ export const BrowserWrapper = styled.div`
       cursor: pointer;
     }
   }
+  .slider {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    flex: 1;
+    
+    .controls {
+      position: absolute;
+      z-index: 1;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      display: flex;
+      justify-content: space-between;
+      bottom: 0;
+      color: #fff;
+
+      .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 83px;
+        height: 100%;
+        cursor: pointer;
+      }
+    }
+
+    .picture {
+      position: relative;
+      height: 100%;
+      overflow: hidden;
+      width: 100%;
+      max-width: 105vh;
+
+      img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        height: 100%;
+        user-select: none;
+      }
+    }
+  }
+
+  .preview {
+    display: flex;
+    justify-content: center;
+    height: 100px;
+    margin-top: 10px;
+  }
 `
