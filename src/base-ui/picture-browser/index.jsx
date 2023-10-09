39ef18a1da7fec70d4b9599203f520aafa8825e5
callie <chzx_dqy@outlook.com>
2023-10-09 13:38:28 +0800

    feat: 详情页面-图片浏览器的底部展示和控制

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
 import React, { memo, useEffect } from "react";
-import { CSSTransition, SwitchTransition } from "react-transition-group"
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import { BrowserWrapper } from "./style";
 import IconClose from "@/assets/svg/icon-close";
 import IconArrowLeft from "@/assets/svg/icon-arrow-left";
 import IconArrowRight from "@/assets/svg/icon-arrow-right";
 import { useState } from "react";
+import IconTriangleArrowBottom from "@/assets/svg/icon-triangle-arrow-bottom";
+import IconTriangleArrowTop from "@/assets/svg/icon-triangle-arrow-top";
+import Indicator from "../indicator";
+import classNames from "classnames";
 
 const PictureBrowser = memo((props) => {
   const { pictureUrls, closeClick } = props;
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isNext, setIsNext] = useState(true);
+  const [showList, setShowList] = useState(true);
   // 当图片浏览器展示出来时, 滚动的功能消失
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -31,8 +36,13 @@ const PictureBrowser = memo((props) => {
     setCurrentIndex(newIndex);
     setIsNext(isNext);
   }
+
+  function bottomItemClickHandle(index) {
+    setIsNext(index > currentIndex);
+    setCurrentIndex(index);
+  }
   return (
-    <BrowserWrapper isNext={isNext} >
+    <BrowserWrapper isNext={isNext} showList={showList}>
       <div className="top">
         <div className="close-btn" onClick={closeBtnClickHandle}>
           <IconClose />
@@ -60,7 +70,43 @@ const PictureBrowser = memo((props) => {
         </div>
       </div>
 
-      <div className="preview"></div>
+      <div className="preview">
+        <div className="info">
+          <div className="desc">
+            <div className="count">
+              <span>
+                {currentIndex + 1}/{pictureUrls.length}:
+              </span>
+              <span>room apartment图片{currentIndex + 1}</span>
+            </div>
+            <div className="toggle" onClick={(e) => setShowList(!showList)}>
+              <span>{showList ? "隐藏" : "显示"}照片列表</span>
+              {showList ? (
+                <IconTriangleArrowBottom />
+              ) : (
+                <IconTriangleArrowTop />
+              )}
+            </div>
+          </div>
+          <div className="list">
+            <Indicator selectIndex={currentIndex}>
+              {pictureUrls.map((item, index) => {
+                return (
+                  <div
+                    className={classNames("item", {
+                      active: currentIndex === index,
+                    })}
+                    key={item}
+                    onClick={(e) => bottomItemClickHandle(index)}
+                  >
+                    <img src={item} alt="" />
+                  </div>
+                );
+              })}
+            </Indicator>
+          </div>
+        </div>
+      </div>
     </BrowserWrapper>
   );
 });
