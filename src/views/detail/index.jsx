f1d5e390ad9771ec6a737b0826b214b4654ed00b
callie <chzx_dqy@outlook.com>
2023-10-09 10:40:40 +0800

    feat: 详情页面-图片的展示和遮盖层效果

--- src/views/detail/index.jsx
+++ src/views/detail/index.jsx
@@ -1,11 +1,19 @@
 import React, { memo } from "react";
 import { useSelector } from "react-redux";
+import { DetailWrapper } from "./style";
+import DetailPictures from "./c-cpns/detail-pictures";
 
 const Detail = memo(() => {
   const { detailInfo } = useSelector((state) => ({
     detailInfo: state.detail.detailInfo,
   }));
-  return <div>{detailInfo.name}</div>;
+  return (
+    <DetailWrapper>
+     
+
+      <DetailPictures/>
+    </DetailWrapper>
+  );
 });
 
 export default Detail;
